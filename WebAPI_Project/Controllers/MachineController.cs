c8d79acd913f2a87288f73b3935635b4d94a0124
nilesh <nileshv@cybage.com>
2023-06-12 19:25:06 +0530

    initial commit

---
+++ WebAPI_Project/Controllers/MachineController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI_Project.Models;
+namespace WebAPI_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MachineController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+            MachineDetails machineDetials = new MachineDetails();
+
+            machineDetials.machineName = Environment.MachineName;
+            machineDetials.machineIP = Request.HttpContext.Connection.RemoteIpAddress.ToString();
+            machineDetials.machineOS = Environment.OSVersion.VersionString;
+            machineDetials.timestamp = DateTime.Now.ToString();
+            return Ok(machineDetials);
+
+
+        }
+
+    }
+}
---
+++ WebAPI_Project/Models/MachineDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAPI_Project.Models
+{
+    public class MachineDetails
+    {
+        public string? machineName { get; set; }
+        public string? machineIP { get; set; }
+        public string? machineOS { get; set; }
+        public string? timestamp { get; set; }
+
+    }
+}
